03012edba81c5152a4f84f1bf7f3c12fd074c830
Ravid Gontov <rvdgntv@gmail.com>
2022-11-27 20:14:13 +0000

    reactivate swagger pace

--- src/main/java/com/ravid/clothes_marketplace/config/SwaggerConfig.java
+++ src/main/java/com/ravid/clothes_marketplace/config/SwaggerConfig.java
@@ -1,5 +1,7 @@
 package com.ravid.clothes_marketplace.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,15 +10,23 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 // Openapi swagger-ui page configuration
 @Configuration
-public class SwaggerConfig 
-{
+public class SwaggerConfig {
+    final String securitySchemeName = "bearerAuth";
+
     @Bean
     OpenAPI customOpenAPI() {
         OpenAPI oApi = new OpenAPI()
-                .components(new Components())
+                .components(new Components().addSecuritySchemes(securitySchemeName,
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .security(List.of(new SecurityRequirement().addList(securitySchemeName)))
                 .info(new Info()
                         .title("Clothes Marketplace")
                         .version("1")
@@ -29,4 +39,14 @@ public class SwaggerConfig
 
         return oApi;
     }
+
+    // @Bean
+    // public OperationCustomizer customize() {
+    // return (operation, handlerMethod) -> operation.addParametersItem(
+    // new Parameter()
+    // .in("header")
+    // .required(true)
+    // .description("bearerToken")
+    // .name("Authorization"));
+    // }
 }
