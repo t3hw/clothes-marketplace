e37454d4a663e7fc042c9977b89dac22e5525bfb
Ravid <44174093+t3hw@users.noreply.github.com>
2022-11-27 02:46:54 +0200

    edit for codespace

--- .devcontainer/devcontainer.sh
+++ .devcontainer/devcontainer.sh
@@ -1,5 +1,5 @@
 if ! [ -r /workspaces/clothes-marketplace-swagger ] ; then
-    git clone git@github.com:t3hw/clothes-marketplace-swagger.git /workspaces/clothes-marketplace-swagger
+#     git clone git@github.com:t3hw/clothes-marketplace-swagger.git /workspaces/clothes-marketplace-swagger
 fi
 
 mvn install -f /workspaces/clothes-marketplace-swagger
