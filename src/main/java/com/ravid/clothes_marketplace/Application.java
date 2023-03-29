e43a174cb528f96c7d63ef2bf64c29211ec08c60
Ravid <121884840+ravidwebz@users.noreply.github.com>
2023-03-29 16:57:45 +0300

    test build cache

--- src/main/java/com/ravid/clothes_marketplace/Application.java
+++ src/main/java/com/ravid/clothes_marketplace/Application.java
@@ -11,9 +11,9 @@ import lombok.extern.log4j.Log4j2;
 public class Application {
 
 	public static void main(String[] args) {
-        log.info( "*********************************** APPLICATION STARTING ************************************");
+        log.info( "************************************ APPLICATION STARTING *************************************");
 		SpringApplication.run(Application.class, args);
-        log.info( "*********************************** APPLICATION LAUNCHED ************************************");
+        log.info( "************************************ APPLICATION LAUNCHED *************************************");
 	}
 
 }
